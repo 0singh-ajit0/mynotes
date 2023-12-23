7fa8281b72753799aff90e794794d62da1563d09
0singh-ajit0 <ajit20@iiitmanipur.ac.in>
2023-12-23 20:39:30 +0530

    Added Bloc support to authentication

---
+++ lib/services/auth/bloc/auth_bloc.dart
@@ -0,0 +1,57 @@
+import 'package:bloc/bloc.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/bloc/auth_event.dart';
+import 'package:mynotes/services/auth/bloc/auth_state.dart';
+
+class AuthBloc extends Bloc<AuthEvent, AuthState> {
+  AuthBloc(AuthProvider provider) : super(const AuthStateLoading()) {
+    // Initialize
+    on<AuthEventInitialize>(
+      (event, emit) async {
+        await provider.initialize();
+        final user = provider.currentUser;
+        if (user == null) {
+          // If the user is not logged in
+          emit(const AuthStateLoggedOut());
+        } else if (!user.isEmailVerified) {
+          // If the user's email is not verified
+          emit(const AuthStateNeedsVerification());
+        } else {
+          // Else, the user is logged in
+          emit(AuthStateLoggedIn(user));
+        }
+      },
+    );
+
+    // Login
+    on<AuthEventLogin>(
+      (event, emit) async {
+        emit(const AuthStateLoading());
+        final email = event.email;
+        final password = event.password;
+        try {
+          final user = await provider.logIn(
+            email: email,
+            password: password,
+          );
+          emit(AuthStateLoggedIn(user));
+        } on Exception catch (e) {
+          emit(AuthStateLoginFailure(e));
+        }
+      },
+    );
+
+    // Logout
+    on<AuthEventLogout>(
+      (event, emit) async {
+        emit(const AuthStateLoading());
+        try {
+          await provider.logOut();
+          emit(const AuthStateLoggedOut());
+        } on Exception catch (e) {
+          emit(AuthStateLogoutFailure(e));
+        }
+      },
+    );
+  }
+}
